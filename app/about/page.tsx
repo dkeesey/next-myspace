50c57c966f3d636dbe3785361e7aedf36cf37037
dkeesey <460435+dkeesey@users.noreply.github.com>
2023-05-16 15:36:56 -0700

    building

---
+++ app/about/page.tsx
@@ -0,0 +1,16 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "About",
+    description: "About page",
+    // ...
+};
+
+export default function About() {
+  return (
+    <main>
+      <h1>About</h1>
+      <p>This is a social media company!</p>
+    </main>
+  );
+}
