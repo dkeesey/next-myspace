1c67b0769967199c825c61b9512f8e21132d90e2
dkeesey <460435+dkeesey@users.noreply.github.com>
2023-05-19 13:06:26 -0700

    following system

--- components/FollowButton/FollowButton.tsx
+++ components/FollowButton/FollowButton.tsx
@@ -1,25 +1,35 @@
 import { getServerSession } from 'next-auth';
 import FollowClient from './FollowClient';
-import { prisma } from '@/lib/prisma';
 import { authOptions } from '../../app/api/auth/[...nextauth]/route'
+
+// import { prisma } from '@/lib/prisma';
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 
 
 interface Props {
   targetUserId: string;
 }
 
-export default async function FollowButton({ targetUserId }: Props) {
-  const session = await getServerSession(authOptions);
 
-  const currentUserId = await prisma.user
-    .findFirst({ where: { email: session?.user?.email! } })
-    .then((user) => user?.id!);
 
-  const isFollowing = await prisma.follows.findFirst({
-    where: { followerId: currentUserId, followingId: targetUserId },
-  });
+export default function FollowButton({ targetUserId }: Props) {
+  const handleFollowButton = async () => {
+    const session = await getServerSession(authOptions);
+    const currentUserId = await prisma.user
+      .findFirst({ where: { email: session?.user?.email! } })
+      .then((user) => user?.id!);
 
-  return (
-    <FollowClient targetUserId={targetUserId} isFollowing={!!isFollowing} />
-  );
+    const isFollowing = await prisma.follows.findFirst({
+      where: { followerId: currentUserId, followingId: targetUserId },
+    });
+
+    // Rest of your code
+
+    return (
+      <FollowClient targetUserId={targetUserId} isFollowing={!!isFollowing} />
+    );
+  };
+
+  return <button onClick={handleFollowButton}>Follow</button>;
 }
